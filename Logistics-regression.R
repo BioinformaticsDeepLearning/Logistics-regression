784e695f5245b3f595dff7c455b22099af3d9143
Alisha Parveen <49845542+aparveen-prog@users.noreply.github.com>
2020-07-22 22:08:50 +0200

    Update Logistics-regression.R

--- Logistics-regression.R
+++ Logistics-regression.R
@@ -1,17 +1,17 @@
-In logistics regression model, dependent variable has categorical variable.
-Therefore, the general mathematical equation are:
-        y = 1/(1+e^-(a+b1x1+b2x2+b3x3+...))
-    Where,
-     y is the dependent variable
-     x is the independent variable
-     a, b is the coefficients
+#In logistics regression model, dependent variable has categorical variable.
+#Therefore, the general mathematical equation are:
+#        y = 1/(1+e^-(a+b1x1+b2x2+b3x3+...))
+#   Where,
+#     y is the dependent variable
+#     x is the independent variable
+#     a, b is the coefficients
 
 #In R, Logistics regression model train by using glm () function#
-Syntax: glm(formula,data,family)
-    Where,
-     formula is creating a relationship between the variables
-     data is the dataset that provides the variables
-     family is R object to specify the details of the model. It's value is binomial for logistic regression.
+# Syntax: glm(formula,data,family)
+#    Where,
+#     formula is creating a relationship between the variables
+#     data is the dataset that provides the variables
+#     family is R object to specify the details of the model. It's value is binomial for logistic regression.
 
 #STEPS INVOLVED IN TRAINING A LOGISTICS MODEL#
 
@@ -23,6 +23,5 @@ Syntax: glm(formula,data,family)
    am.data = glm(formula=am ~ cyl + hp + wt ,data=input,family=binomial) #creating relationship between am with cyl+hp+wt#
    print(summary(am.data))
    
-Note: In the output, user will find p value of the model. If Pvalue is more than 0.05, then we consider them to be insignificant in contributing to the value of the variable
-"am". Therefore, weight (wt) impacts the "am" value in this regression model.
+#Note: In the output, user will find p value of the model. If Pvalue is more than 0.05, then we consider them to be insignificant in contributing to the value of the variable "am". Therefore, weight (wt) impacts the "am" value in this regression model.
 
